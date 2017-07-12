12aef2010d9924918b1b9b983730f250c15b7fc8
maciejrkowalski <maciej.r.kowalski@gmail.com>
2017-07-12 20:30:20 +0200

    Dodanie klasy Klasa

---
+++ src/pl/sdacademy/exercises0407/Klasa.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.exercises0407;
+
+public class Klasa {
+
+    private int a;
+
+    public Klasa(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
+
